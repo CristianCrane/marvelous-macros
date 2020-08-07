0fc283c334c946642f5d1a28b5fd84bc1cd9d760
Cris <cristiandcrane@gmail.com>
2020-08-07 01:21:22 -0400

    add springboot starter

---
+++ api/src/main/java/com/marvelousmacros/api/MarvelousmacrosApplication.java
@@ -0,0 +1,13 @@
+package com.marvelousmacros.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelousmacrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelousmacrosApplication.class, args);
+	}
+
+}
